f06ae557f6552afbf3e988a0c102db1cffb4258a
deezitheviper <amuhdavid@gmail.com>
2022-11-29 13:23:59 +0100

    header component done for

--- src/sections/header/Header.js
+++ src/sections/header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './header.scss'
-
+import image from '../../assets/img/mind.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='zigi_header section_padding' id="home">
+     <div className='content'>
+      <div className='container'>
+      <h1 className='gradient_text'>AI Designs Quantum Physics Experiments beyond What Any Human Has Conceived</h1>     
+      <p>Revolutionary artificial intelligence makes quantum physics 99.99% simpler</p>
+      </div>
+      <div className='image'>
+          <img src={image} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
