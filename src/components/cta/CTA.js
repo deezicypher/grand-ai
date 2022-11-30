36ce3e8cdd04e6e8aa9e57e6aff0a49ea1666560
deezitheviper <amuhdavid@gmail.com>
2022-11-30 09:44:50 +0100

    call to action button

--- src/components/cta/CTA.js
+++ src/components/cta/CTA.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './cta.scss';
+
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='deezi_cta section_padding'>
+        <div className='content'>
+          <p>If you’d like to learn more </p>
+          <h3 className='gradient_text'>It's Frictionless, Register Today to learn more</h3>
+        </div>
+        <div className='cta_button'>
+          <button type='button'>Get Started</button>
+        </div>
+    </div>
   )
 }
 
