cd868147694b317eed6eaa32751a032a1fe46169
deezitheviper <amuhdavid@gmail.com>
2022-12-22 17:34:02 +0100

    update for header and navbar

--- src/sections/header/Header.js
+++ src/sections/header/Header.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import './header.scss'
 import image from '../../assets/img/mind.png'
+import Typewriter from 'typewriter-effect';
 
 const Header = () => {
   return (
     <div className='deezi_header section_padding' id="home">
      <div className='content'>
       <div className='container'>
-      <h1 className='gradient_text'>AI Designs Quantum Physics Experiments beyond What Any Human Has Conceived</h1>     
-      <p>Revolutionary artificial intelligence makes quantum physics 99.99% simpler</p>
+
+      <Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString("<h1>AI Designs Quantum Physics Experiments beyond What Any Human Has Conceived</h1>")
+      .pauseFor(2500)
+      .typeString(" <p>Revolutionary artificial intelligence makes quantum physics 99.99% simpler</p>")
+      .start()
+  }}
+/>   
+     
       </div>
       <div className='image'>
           <img src={image} alt="" />
